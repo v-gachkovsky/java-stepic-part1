4e8f332ea4bd660574fbb01aa2a1dab6b114f215
v-gachkovsky <v.gachkovsky@gmail.com>
2017-06-27 00:21:11 +0300

    hw01 done

---
+++ src/main/java/servlets/MirrorRequestServlet.java
@@ -0,0 +1,30 @@
+package servlets;
+
+import templater.PageGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MirrorRequestServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+
+        Map<String, Object> pageVariables = createPageVariablesMap(request);
+
+        response.getWriter().println(PageGenerator.instance().getPage("mirror.html", pageVariables));
+
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    private static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("key", request.getParameter("key"));
+        return pageVariables;
+    }
+}
